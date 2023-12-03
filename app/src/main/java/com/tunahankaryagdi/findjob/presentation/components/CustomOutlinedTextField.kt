0f5b64c83a51f2f7ba544eaedccd4ff8bda065df
Tunahan Karyagdi <thankaryagdi@gmail.com>
2023-12-03 22:28:43 +0300

    fix:drawer and text field ui fix

--- app/src/main/java/com/tunahankaryagdi/findjob/presentation/components/CustomOutlinedTextField.kt
+++ app/src/main/java/com/tunahankaryagdi/findjob/presentation/components/CustomOutlinedTextField.kt
@@ -1,6 +1,9 @@
 package com.tunahankaryagdi.findjob.presentation.components
 
+
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -8,7 +11,6 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.tunahankaryagdi.findjob.ui.theme.CustomTheme
@@ -21,22 +23,25 @@ fun CustomOutlinedTextField(
     value: String,
     onValueChange: (String) -> Unit,
     leadingIcon: @Composable (() -> Unit)? = null,
-    trailingIcon: @Composable() (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
     size : Dp = 50.dp,
     singleLine : Boolean = true,
     placeholder: String
 ) {
+
+
     OutlinedTextField(
-        modifier = modifier
-            .height(size),
+        modifier = modifier,
         value = value,
+        textStyle = CustomTheme.typography.bodySmall,
         onValueChange = onValueChange,
         colors = TextFieldDefaults.outlinedTextFieldColors(
             containerColor = CustomTheme.colors.secondaryBackground,
             focusedBorderColor = Color.Transparent,
             disabledBorderColor = Color.Transparent,
             errorBorderColor = Color.Transparent,
-            unfocusedBorderColor = Color.Transparent
+            unfocusedBorderColor = Color.Transparent,
+            cursorColor = CustomTheme.colors.primary
         ),
         placeholder = {
             Text(text = placeholder)
--- app/src/main/java/com/tunahankaryagdi/findjob/presentation/home/components/AppDrawer.kt
+++ app/src/main/java/com/tunahankaryagdi/findjob/presentation/home/components/AppDrawer.kt
@@ -1,13 +1,21 @@
 package com.tunahankaryagdi.findjob.presentation.home.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,8 +23,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.tunahankaryagdi.findjob.R
+import com.tunahankaryagdi.findjob.presentation.components.SpacerHeight
+import com.tunahankaryagdi.findjob.ui.theme.CustomTheme
 import kotlinx.coroutines.launch
 
 
@@ -45,22 +61,66 @@ fun AppDrawer(
     val scope  = rememberCoroutineScope()
 
     ModalNavigationDrawer(
+        modifier = modifier,
         drawerState = drawerState,
         drawerContent = {
-            ModalDrawerSheet {
-                items.forEachIndexed { index, drawerItem ->
-                    NavigationDrawerItem(
-                        label = {Text(drawerItem.title)},
-                        selected = index == selectedIndex,
-                        onClick = {
-                            selectedIndex = index
-                            scope.launch {
-                                drawerState.close()
-                            }
-                        }
+            ModalDrawerSheet(
+                drawerContainerColor = CustomTheme.colors.secondaryBackground,
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            NavigationDrawerItemDefaults.ItemPadding
+                        ),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    SpacerHeight(size = CustomTheme.spaces.medium)
+
+                    Image(
+                        modifier = Modifier
+                            .clip(CircleShape),
+                        painter = painterResource(id = R.drawable.ic_launcher_background),
+                        contentDescription = stringResource(id = R.string.job_image)
                     )
+
+                    SpacerHeight(size = CustomTheme.spaces.small)
+
+                    Text(
+                        text = "Screen1",
+                        style = CustomTheme.typography.titleLarge
+                    )
+
+
+                    Text(
+                        text = "Screen2",
+                        style = CustomTheme.typography.labelLarge
+                    )
+
+                    SpacerHeight(size = CustomTheme.spaces.large)
+
+                    items.forEachIndexed { index, drawerItem ->
+                        NavigationDrawerItem(
+                            label = {Text(drawerItem.title)},
+                            selected = index == selectedIndex,
+                            onClick = {
+                                selectedIndex = index
+                                scope.launch {
+                                    drawerState.close()
+                                }
+                            },
+                            icon = {
+                                Icon(imageVector = drawerItem.icon, contentDescription = drawerItem.title)
+                            },
+                            colors = NavigationDrawerItemDefaults.colors(
+                                selectedContainerColor = Color.Transparent,
+                                unselectedContainerColor = Color.Transparent,
+                            ),
+                        )
+                    }
                 }
-            }
+       }
         }
     ) {
         content()
